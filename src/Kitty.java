4f71fb0959d93221762d5f7af95fc94353def9c8
SardarJava <sardar.rmfc@gmail.com>
2022-07-18 00:24:36 +0300

    Initial commit

---
+++ src/Kitty.java
@@ -0,0 +1,6 @@
+public class Kitty  extends Cat {
+
+        public Kitty(String name, int age, double weight) {
+            super(name, age, weight);
+        }
+    }
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Tom", 6, 9.800);
+        Kitty kitty = new Kitty("Little Tommy", 1, 1.259);
+        System.out.println(cat);
+        System.out.println(kitty);
+    }
+}
